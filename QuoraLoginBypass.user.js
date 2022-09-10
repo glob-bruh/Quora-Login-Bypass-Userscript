5a3a06ae2ee2e7262a81a2d122691ed077d93b1a
glob-bruh <cirethe101@gmail.com>
2022-09-10 01:10:14 -0600

    Add whitelist functionality.

--- QuoraLoginBypass.user.js
+++ QuoraLoginBypass.user.js
@@ -8,8 +8,31 @@
 // @description Allows you to view multiple Quora pages without logging in or signing up.
 // ==/UserScript==
 
+// Whitelisted URL's:
+const whitelistURL = [
+    "https://www.quora.com/",
+    "https://www.quora.com/about*",
+    "https://www.quora.com/careers*",
+    "https://www.quora.com/press*"
+]
+
 var url = window.location.href;
-if (url.indexOf("?share=1") == -1 && url != "https://www.quora.com/") {
+var changeURL = true;
+for (let i = 0, constLen = whitelistURL.length ; i < constLen ; i++) {
+
+    if (whitelistURL[i].slice(-1) == "*") {
+        if (RegExp(whitelistURL[i]).test(url)) {
+            changeURL = false
+        }
+    } else {
+        if (url == whitelistURL[i]) {
+            changeURL = false
+        }
+    }
+}
+if (changeURL == false || url.indexOf("?share=1") != -1) {
+    return false
+} else {
     url += "?share=1";
     window.location.replace(url)
 }
